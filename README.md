a37960c9929a0c83ea448fc1bd7780a81693d22b
Lê Quốc Anh <66353897+lequocanh20@users.noreply.github.com>
2021-03-15 21:55:35 +0700

    Create entity classes and setup EF Core
    fixes #3

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 # Technologies
 - ASP .Net Core 3.1
 - Entity Framework Core 3.1
+## Install Packages
+- Microsoft.EntityFrameworkCore.SqlServer
+- Microsoft.EntityFrameworkCore.Tools
+- Microsoft.EntityFrameworkCore.Design
 
---
+++ eShopSolution.Data/Entities/Category.cs
@@ -0,0 +1,20 @@
+﻿using eShopSolution.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public int SortOrder { get; set; }
+
+        public bool IsShowOnHome { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
