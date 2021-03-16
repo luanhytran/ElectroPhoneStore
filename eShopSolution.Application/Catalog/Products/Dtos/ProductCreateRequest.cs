ab9b432858b91d5b854569536167fea27d24a6c1
luanhytran <hytranluan@gmail.com>
2021-03-17 00:01:40 +0700

    - Create application layer : manage and public service that crud product - Create some view model

---
+++ eShopSolution.Application/Catalog/Products/Dtos/ProductCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products.Dtos
+{
+    public class ProductCreateRequest
+    {
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ eShopSolution.Application/Dtos/PagedViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Dtos
+{
+    public class PagedViewModel<T>
+    {
+        public List<T> Items { get; set; }
+        public int TotalRecord { get; set; }
+    }
+}
