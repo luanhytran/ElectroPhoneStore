27396f378a0a52e63d2ab687abb4af7f6f3ea3af
luanhytran <hytranluan@gmail.com>
2021-03-19 13:32:56 +0700

    - Add API for manage product method ( API for ManageProductService method ) - Change some IManage and IPublic parameter, and change some implemeted method of these interface

--- eShopSolution.Application/Catalog/Products/PublicProductService.cs
+++ eShopSolution.Application/Catalog/Products/PublicProductService.cs
@@ -21,31 +21,34 @@ namespace eShopSolution.Application.Catalog.Products
             _context = context;
         }
 
-        public async Task<List<ProductViewModel>> GetAll()
+        public async Task<List<ProductViewModel>> GetAll(string languageId)
         {
+            // 1.Select join
             var query = from p in _context.Products
                         join pt in _context.ProductTranslations on p.Id equals pt.ProductId
                         join pic in _context.ProductInCategories on p.Id equals pic.ProductId
                         join c in _context.Categories on pic.CategoryId equals c.Id
+                        where pt.LanguageId == languageId
                         // select nhiều  đối tượng thì mình ghi new { các đối tượng ta muốn select }
                         select new { p, pt, pic };
 
             var data = await query.Select(x => new ProductViewModel()
-                            {
-                                Id = x.p.Id,
-                                Name = x.pt.Name,
-                                DateCreated = x.p.DateCreated,
-                                Description = x.pt.Description,
-                                Details = x.pt.Details,
-                                LanguageId = x.pt.LanguageId,
-                                OriginalPrice = x.p.OriginalPrice,
-                                Price = x.p.Price,
-                                SeoAlias = x.pt.SeoAlias,
-                                SeoDescription = x.pt.SeoDescription,
-                                SeoTitle = x.pt.SeoTitle,
-                                Stock = x.p.Stock,
-                                ViewCount = x.p.ViewCount
-                            }).ToListAsync();
+            {
+                Id = x.p.Id,
+                Name = x.pt.Name,
+                DateCreated = x.p.DateCreated,
+                Description = x.pt.Description,
+                Details = x.pt.Details,
+                LanguageId = x.pt.LanguageId,
+                OriginalPrice = x.p.OriginalPrice,
+                Price = x.p.Price,
+                SeoAlias = x.pt.SeoAlias,
+                SeoDescription = x.pt.SeoDescription,
+                SeoTitle = x.pt.SeoTitle,
+                Stock = x.p.Stock,
+                ViewCount = x.p.ViewCount
+            }).ToListAsync();
+
             return data;
         }
 
@@ -55,6 +58,7 @@ namespace eShopSolution.Application.Catalog.Products
                         join pt in _context.ProductTranslations on p.Id equals pt.ProductId
                         join pic in _context.ProductInCategories on p.Id equals pic.ProductId
                         join c in _context.Categories on pic.CategoryId equals c.Id
+                        where pt.LanguageId == request.LanguageId
                         // select nhiều  đối tượng thì mình ghi new { các đối tượng ta muốn select }
                         select new { p, pt, pic };
 
--- eShopSolutionBackendApi/Controllers/ProductController.cs
+++ eShopSolutionBackendApi/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using eShopSolution.Application.Catalog.Products;
+using eShopSolution.ViewModels.Catalog.Products;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,15 +14,95 @@ namespace eShopSolutionBackendApi.Controllers
     public class ProductController : ControllerBase
     {
         private readonly IPublicProductService _publicProductService;
-        public ProductController(IPublicProductService publicProductService)
+        private readonly IManageProductService _manageProductService;
+
+        public ProductController(IPublicProductService publicProductService, IManageProductService manageProductService)
         {
             _publicProductService = publicProductService;
+            _manageProductService = manageProductService;
         }
-        [HttpGet]
-        public async Task<IActionResult> Get()
+
+
+        //http://localhost:port/product
+        [HttpGet("{languageId}")]
+        public async Task<IActionResult> Get(string languageId)
         {
-            var products = await _publicProductService.GetAll();
-            return Ok(products);
+            var product = await _publicProductService.GetAll(languageId);
+            return Ok(product);
+        }
+
+
+        //http://localhost:port/product/public-paging
+        [HttpGet("public-paging/{languageId}")]
+        public async Task<IActionResult> Get([FromForm] GetPublicProductPagingRequest request)
+        {
+            var product = await _publicProductService.GetAllByCategoryId(request);
+            return Ok(product);
+        }
+
+
+        [HttpGet("{id}/{languageId}")]
+        public async Task<IActionResult> GetById(int id, string languageId)
+        {
+            var product = await _manageProductService.GetById(id, languageId);
+            if (product == null)
+                return BadRequest("Cannot find product");
+            return Ok(product);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] ProductCreateRequest request)
+        {
+            var productId = await _manageProductService.Create(request);
+            if (productId == 0)
+            {
+                return BadRequest();
+            }
+
+            var product = await _manageProductService.GetById(productId, request.LanguageId);
+
+            return CreatedAtAction(nameof(GetById), new { id = productId }, product);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromForm] ProductUpdateRequest request)
+        {
+            var affectedResult = await _manageProductService.Update(request);
+            if (affectedResult == 0)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
+
+
+        [HttpDelete("{productId}")]
+        public async Task<IActionResult> Delete(int productId)
+        {
+            var affectedResult = await _manageProductService.Delete(productId);
+            if (affectedResult == 0)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
+
+        [HttpPut("price/{id}/{newPrice}")]
+        public async Task<IActionResult> UpdatePrice(int id, decimal newPrice)
+        {
+            var isSuccessful = await _manageProductService.UpdatePrice(id, newPrice);
+            if (isSuccessful)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
         }
     }
 }
