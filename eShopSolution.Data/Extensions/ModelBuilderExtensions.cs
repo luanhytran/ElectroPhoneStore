bf8b999078ef3342441c8401d3d12fcc523d8d87
luanhytran <hytranluan@gmail.com>
2021-03-19 12:25:25 +0700

    - change some seed data in migration SeedData - add swagger api

--- eShopSolution.Data/Extensions/ModelBuilderExtensions.cs
+++ eShopSolution.Data/Extensions/ModelBuilderExtensions.cs
@@ -19,7 +19,7 @@ namespace eShopSolution.Data.Extentions
                );
 
             modelBuilder.Entity<Language>().HasData(
-                new Language() { Id = "vi-vn", Name = "Tiếng Việt", IsDefault = true },
+                new Language() { Id = "vi-VN", Name = "Tiếng Việt", IsDefault = true },
                 new Language() { Id = "en-US", Name = "English", IsDefault = false }
                 );
 
@@ -45,9 +45,9 @@ namespace eShopSolution.Data.Extentions
               );
 
             modelBuilder.Entity<CategoryTranslation>().HasData( 
-                  new CategoryTranslation() { Id = 1, CategoryId=1, Name = "Áo nam", LanguageId="vi-vn", SeoAlias="ao-nam",SeoDescription="Sản phẩm áo thời trang nam",SeoTitle= "Sản phẩm áo thời trang nam" },
+                  new CategoryTranslation() { Id = 1, CategoryId=1, Name = "Áo nam", LanguageId="vi-VN", SeoAlias="ao-nam",SeoDescription="Sản phẩm áo thời trang nam",SeoTitle= "Sản phẩm áo thời trang nam" },
                   new CategoryTranslation() { Id = 2, CategoryId = 1, Name = "Men Shirt", LanguageId="en-US", SeoAlias="men-shirt",SeoDescription="The shirt product for men",SeoTitle= "The shirt product for men" },
-                  new CategoryTranslation() { Id = 3, CategoryId = 2, Name = "Áo nữ", LanguageId = "vi-vn", SeoAlias = "ao-nu", SeoDescription = "Sản phẩm áo thời trang nữ", SeoTitle = "Sản phẩm áo thời trang nữ" },
+                  new CategoryTranslation() { Id = 3, CategoryId = 2, Name = "Áo nữ", LanguageId = "vi-VN", SeoAlias = "ao-nu", SeoDescription = "Sản phẩm áo thời trang nữ", SeoTitle = "Sản phẩm áo thời trang nữ" },
                   new CategoryTranslation() { Id = 4, CategoryId = 2, Name = "Women Shirt", LanguageId = "en-US", SeoAlias = "women-shirt", SeoDescription = "The shirt product for women", SeoTitle = "The shirt product for women" }
               );
 
@@ -64,26 +64,27 @@ namespace eShopSolution.Data.Extentions
             modelBuilder.Entity<ProductTranslation>().HasData(
                   new ProductTranslation()
                   {
-                      Id=1,
-                      ProductId=1,
-                      Name = "Áo sơ mi nam trắng",
-                      LanguageId = "vi-vn",
-                      SeoAlias = "ao-so-mi",
-                      SeoDescription = "Áo sơ mi nam trắng",
-                      SeoTitle = "Áo sơ mi nam trắng",
-                      Details = "Áo sơ mi nam trắng"
+                      Id = 1,
+                      ProductId = 1,
+                      Name = "Áo sơ mi nam trắng Việt Tiến",
+                      LanguageId = "vi-VN",
+                      SeoAlias = "ao-so-mi-nam-trang-viet-tien",
+                      SeoDescription = "Áo sơ mi nam trắng Việt Tiến",
+                      SeoTitle = "Áo sơ mi nam trắng Việt Tiến",
+                      Details = "Áo sơ mi nam trắng Việt Tiến",
+                      Description = "Áo sơ mi nam trắng Việt Tiến"
                   },
                   new ProductTranslation()
                   {
-                      Id=2,
-                      ProductId=1,
-                      Name = "Men T-Shirt",
+                      Id = 2,
+                      ProductId = 1,
+                      Name = "Viet Tien Men T-Shirt",
                       LanguageId = "en-US",
-                      SeoAlias = "Men T-Shirt",
-                      SeoDescription = "Men T-Shirt",
-                      SeoTitle = "Men T-Shirt",
-                      Details = "Men T-Shirt",
-                      Description = ""
+                      SeoAlias = "viet-tien-men-t-shirt",
+                      SeoDescription = "Viet Tien Men T-Shirt",
+                      SeoTitle = "Viet Tien Men T-Shirt",
+                      Details = "Viet Tien Men T-Shirt",
+                      Description = "Viet Tien Men T-Shirt"
                   });
             modelBuilder.Entity<ProductInCategory>().HasData(
                 new ProductInCategory() { ProductId = 1, CategoryId = 1 }
--- eShopSolutionBackendApi/Startup.cs
+++ eShopSolutionBackendApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,6 +34,11 @@ namespace eShopSolutionBackendApi
             //Declare DI
             services.AddTransient<IPublicProductService, PublicProductService>();
             services.AddControllersWithViews();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger eShop Solution", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +60,13 @@ namespace eShopSolutionBackendApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger eShopSolution V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
