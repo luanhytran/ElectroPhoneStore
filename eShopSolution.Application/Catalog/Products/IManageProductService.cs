5604db186d60f60f444e686d2538266744f9e395
luanhytran <hytranluan@gmail.com>
2021-03-20 18:57:59 +0700

    Add GetById Interface

--- eShopSolution.Application/Catalog/Products/IManageProductService.cs
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -23,6 +23,8 @@ namespace eShopSolution.Application.Catalog.Products
         // để xóa thì ta chỉ cần truyền vào 1 product id
         Task<int> Delete(int productId);
 
+        Task<ProductViewModel> GetById(int productId, string languageId);
+
         Task<bool> UpdatePrice(int productId, decimal newPrice);
 
         Task<bool> UpdateStock(int productId, int addedQuantity);
