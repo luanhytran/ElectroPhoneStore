64da9f0b9c2651b0233e1ff2548264f8f2686357
luanhytran <47807877+luanhytran@users.noreply.github.com>
2021-04-08 09:32:05 +0700

    Add validation for update product and update user

---
+++ eShopSolution.ViewModels/Catalog/Products/ProductUpdateRequestValidation.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.ViewModels.Catalog.Products
+{
+    public class ProductUpdateRequestValidation : AbstractValidator<ProductUpdateRequest>
+    {
+        public ProductUpdateRequestValidation()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Tên sản phẩm không được để trống")
+               .MaximumLength(200).WithMessage("Tên không được quá 200 ký tự");
+
+            RuleFor(x => x.Description)
+               .MaximumLength(1000).WithMessage("Mô tả không được quá 1000 ký tự");
+
+            RuleFor(x => x.Details)
+                .MaximumLength(10000).WithMessage("Mô tả chi tiết không được quá 10000 ký tự");
+
+            RuleFor(x => x.SeoDescription)
+                .MaximumLength(200).WithMessage("Mô tả Seo không được quá 200 ký tự");
+
+            RuleFor(x => x.SeoAlias)
+                .MaximumLength(200).WithMessage("Bí danh Seo không được quá 200 ký tự");
+
+            RuleFor(x => x.SeoTitle)
+                .MaximumLength(200).WithMessage("Tiêu đề Seo không được quá 200 ký tự");
+        }
+    }
+}
---
+++ eShopSolution.ViewModels/System/Users/UserUpdateRequestValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.ViewModels.System.Users
+{
+    public class UserUpdateRequestValidator : AbstractValidator<UserUpdateRequest>
+    {
+        public UserUpdateRequestValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("Tên là bắt buộc")
+               .MaximumLength(200).WithMessage("Tên không được quá 200 ký tự");
+
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("Họ là bắt buộc")
+                .MaximumLength(200).WithMessage("Họ không được quá 200 ký tự");
+
+            RuleFor(x => x.Dob).GreaterThan(DateTime.Now.AddYears(-100))
+                .WithMessage("Năm sinh không thể lớn hơn 100 năm");
+
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email là bắt buộc")
+                .Matches(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")
+                .WithMessage("Định dạng Email không đúng");
+
+            RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Số điện thoại là bắt buộc");
+        }
+    }
+}
