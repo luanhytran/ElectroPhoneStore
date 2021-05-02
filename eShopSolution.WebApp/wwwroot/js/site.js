5d6eb4c3861a52d28d7fa525770f6812dc1aa88e
luanhytran <47807877+luanhytran@users.noreply.github.com>
2021-05-02 17:20:31 +0700

    fixes #47

--- eShopSolution.WebApp/wwwroot/js/site.js
+++ eShopSolution.WebApp/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('body').on('click', '.btn-add-cart', function (e) {
+    e.preventDefault(); // line này để khi bấm ok (alert) thì không bị nhảy lên top website
+
+    const culture = $('#hidCulture').val();
+    const id = $(this).data('id');
+
+    $.ajax({  // xử lý khi click thêm Product vào Cart
+        type: "POST",
+        url: '/' + culture + '/Cart/AddToCart',
+        data: {
+            id: id,
+            languageId: culture
+        },
+        success: function (res) {
+            console.log(res)
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+})
