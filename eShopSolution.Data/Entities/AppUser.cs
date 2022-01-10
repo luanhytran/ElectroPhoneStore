2cbca0edb4f358c4f308b0bbc54533dfa0ae8415
luanhytran <47807877+luanhytran@users.noreply.github.com>
2022-01-10 12:53:00 +0700

    Update not allow name in Proxy Pattern

--- eShopSolution.Data/Entities/AppUser.cs
+++ eShopSolution.Data/Entities/AppUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace eShopSolution.Data.Entities
@@ -38,10 +39,10 @@ namespace eShopSolution.Data.Entities
 
         public async Task<RegisterState> UpdateDatabase(UserManager<AppUser> userManager, string password)
         {
-            List<string> notAllowName = new List<string>() {
-                "Ho Chi Minh",
-                "Vo Nguyen Giap"
-            };
+            var LOG_PATH = @"..\eShopSolution.Data\NotAllowName.txt";
+            List<string> notAllowName = new List<string>();
+            var logFile = File.ReadAllLines(LOG_PATH);
+            foreach (var s in logFile) notAllowName.Add(s);
 
             foreach (var name in notAllowName)
             {
